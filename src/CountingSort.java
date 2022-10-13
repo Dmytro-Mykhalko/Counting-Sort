7645163046299a9dd66bfc84e722369930faaffe
Dmytro Mykhalko <d.mykhalko@gmail.com>
2022-10-13 20:11:43 +0300

    Adding the actual code. Wrong data was added in the previous commit.

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -1,31 +1,25 @@
 import java.util.Random;
 
-public class Main {
+/*
+* I chose to use Counting Sort because it has linear complexity
+* and is perfectly suitable to sort objects according to the keys that are small numbers (n > k)
+*
+* In our task we had an int array of 2,147,483,645 length with randomly distributed values from 0 to 999,
+*
+* */
+public class CountingSort {
 
-    public static void main(String[] args) {
-        int volume = Integer.MAX_VALUE - 2;
-        int maxValue = 999;
-        int[] nums = createAndFillArray(volume, maxValue);
-
-        System.out.println("Before:");
-        for (int num: nums) System.out.printf("%s,", num);
-
-        customCountingSort(nums, 999);
-
-        System.out.println("After:");
-        for (int num: nums) System.out.printf("%s,", num);
-    }
-
-    // Changed the Counting Sort to avoid using extra array for sorted values
-    private static void customCountingSort(int[] arr, int maxValue) {
-
-        // initializing array that consist of counting the amount of repeating values [0; 999]
+    /*
+    * Changed the Counting Sort to avoid using extra array for sorted values
+    * because there would be huge data usage.
+    *
+    * I have added original Counting Sort algorithm below
+    * */
+    private static void customSort(int[] arr, int maxValue) {
         int[] count = new int[maxValue + 1];
 
-        // filling the COUNT ARRAY with the count of repeated values
         for (int k : arr) ++count[k];
 
-        // filling arr with sorted values
         for (int i = 0, j = 0; i < arr.length && j < count.length ; i++, j++) {
             if (count[j] > 0) {
                 arr[i] = j;
@@ -35,7 +29,44 @@ public class Main {
         }
     }
 
-    public static int[] createAndFillArray(int volume, int maxValue) {
+    /*
+    * Default implementation of Counting Sort.
+    * As a disadvantage it uses a huge amount of data because of creating an output array
+    * that should contain the same amount of elements as an inout one
+    * */
+    public static void sort(int[] input, int maxValue) {
+        int[] output = new int[input.length];
+        int[] count = new int[maxValue + 1];
+
+        for (int k : input) ++count[k];
+
+        for (int i = 0; i < count.length - 1; i++)
+            count[i + 1] += count[i];
+
+        for (int i = 0; i < output.length ; i++) {
+            output[count[input[i]] - 1] = input[i];
+            --count[input[i]];
+        }
+
+        for (int i = 0; i < output.length; i++)
+            input[i] = output[i];
+    }
+
+    public static void main(String[] args) {
+        int volume = Integer.MAX_VALUE - 2;
+        int maxValue = 999;
+        int[] nums = createAndFillArray(volume, maxValue);
+
+        System.out.println("Before:");
+        for (int num: nums) System.out.printf("%s,", num);
+
+        customSort(nums, 999);
+
+        System.out.println("After:");
+        for (int num: nums) System.out.printf("%s,", num);
+    }
+
+    private static int[] createAndFillArray(int volume, int maxValue) {
         Random random = new Random();
         int[] result = new int[volume];
 
@@ -44,6 +75,4 @@ public class Main {
 
         return result;
     }
-
-    
 }
